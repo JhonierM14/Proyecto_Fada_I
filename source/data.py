9cb859adbeb5c36027379f53e27a755d7a50cf82
David Santiago Guerrero <144239804+DavidSantiagoGuerrero@users.noreply.github.com>
2025-07-21 16:59:44 -0500

    Update data.py
    Añadido progreso para leer archivos txt

--- source/data.py
+++ source/data.py
@@ -1,3 +1,4 @@
+from data_structures.listadoble import *
 from form.encuestado import Encuestado
 from form.pregunta import Pregunta
 from form.tema import Tema
@@ -30,3 +31,76 @@ tema2 = Tema("Tema 2", [pregunta2_1, pregunta2_2])
 
 # numero de temas, numero de preguntas, cantidad minima de encuestados, cantidad maxima de encuestados, temas
 prueba = Encuesta(2, 2, 2, 4, [tema1, tema2])
+
+
+
+def Encuestado_a_Objeto(texto, id):
+    partes = texto.split(',')
+    nombre = partes[0]
+    experiencia = partes[1].split(': ')[1]
+    opinion = partes[2].split(': ')[1]
+    return Encuestado(id, nombre, int(experiencia), int(opinion))
+
+def Pregunta_a_Objeto(texto, nombre, encuestados_total):
+    partes = texto.strip('{}')
+    partes = partes.split(', ')
+    encuestados_pregunta = None
+    for e in partes:
+        encuestados_pregunta = List_Insert_End(encuestados_pregunta, encuestados_total[int(e)])
+    return Pregunta(nombre, encuestados_pregunta)
+
+# Pregunta_a_Objeto("{10, 3, 2}", id)
+
+# print(Encuestado_a_Objeto("Sofía García, Experticia: 1, Opinión: 6", 1).getID())
+# print(Encuestado_a_Objeto("Sofía García, Experticia: 1, Opinión: 6", 1).getNombre())
+# print(Encuestado_a_Objeto("Sofía García, Experticia: 1, Opinión: 6", 1).getExperticia())
+# print(Encuestado_a_Objeto("Sofía García, Experticia: 1, Opinión: 6", 1).getOpinion())
+
+def Texto_a_Encuesta(archivo):
+    with open("Proyecto_FADA_I/Pruebas/" + archivo, "r", encoding='utf-8') as documento:
+        texto = documento.read()
+        parrafos = texto.split("\n\n")
+        encuestados = texto.split("\n\n")[0].split("\n")
+        sada = []
+        contador_encuestados = 1
+        lista_encuestados = None
+        lista_temas = None
+
+        for i in range(0, len(parrafos)):
+            if i == 0:
+                for e in encuestados:
+                    sada.append(e)
+                    lista_encuestados = List_Insert_End(lista_encuestados, e)
+                    contador_encuestados += 1
+                    #lista_encuestados = List_Insert_End(lista_encuestados, Encuestado_a_Objeto(e, contador_encuestados))
+            else:
+                lista_preguntas = None
+                for j in range(1, len(parrafos[i].split("\n"))):
+                    #print(parrafos[i].split("\n")[j])
+                    print("Pregunta: " + str(i+j*0.1))
+                #print(parrafos[i].split("\n"))
+                tema = Tema("Tema " + str(i), [])
+                lista_temas = List_Insert_End(lista_temas, tema)
+
+    #return Encuesta(len(parrafos)-1, len(parrafos[i].split("\n"))-1, 1, 1, lista_temas)
+
+Texto_a_Encuesta("Test2.txt")
+    #print(sada)
+    #encuesta = Encuesta(1, 1, 1, 1, [])
+
+    # print(texto.split("\n\n")[1].split("\n"))
+    # print(texto.split("\n\n")[2].split("\n"))
+    
+    #List_Print(lista_encuestados)
+
+    # encuestados = None
+    # temas = None
+    # preguntas = None
+    # for x in texto:
+    #     linea = texto.readline()
+    #     print(linea)
+    #     if linea.startswith('{'):
+    #         preguntas = List_Insert(preguntas, linea.strip())
+    #     else:
+    #         encuestados = List_Insert_End(encuestados, linea.strip())
+    # List_Print(encuestados)
