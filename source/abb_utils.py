af60ce6eaf5fb51b5507e76180fc91e8c8504917
juanpab05 <165406198+juanpab05@users.noreply.github.com>
2025-07-22 15:56:26 -0500

    punto 1 abb_utils.py
    Agregue el punto 1 usando ABB, para que lo usen como inspiracion

--- source/abb_utils.py
+++ source/abb_utils.py
@@ -2,8 +2,67 @@ from controlador_abb import *
 
 #Punto 1
 
-def punto1_Abb():
-    pass
+#Punto 1 con ABB:
+lista: list = #Ingrese la lista de encuestados
+
+#Funcion propia que reemplaza el len de python
+def cant(lst):
+    a=0
+    while lst:
+        a=a+1
+        lst.pop(0)
+    return a 
+
+#Funcion que retorna el abb con los encuestados ya ordenados
+def abb_encuestados(lst):
+    i=1
+    root=abb(lst[0])
+    while i<cant(copy.copy(lst)):
+        abb_insert_encuestados(root, lst[i])
+        i=i+1
+    return root
+
+#Insertar los encuestados en un abb segun su opinion, o experticia en caso de empate
+def abb_insert_encuestados(root, key):
+    if root is None:
+        return abb(key)
+    
+    if root.val.getOpinion()>key.getOpinion():
+        if root.left == None:
+            root.left = abb(key)
+        else:
+           abb_insert_encuestados(root.left, key) 
+     
+    if root.val.getOpinion()==key.getOpinion():
+        if root.val.getExperticia()>=key.getExperticia():
+            if root.left == None:
+                root.left = abb(key)
+            else:
+               abb_insert_encuestados(root.left, key) 
+        else:
+           if root.right == None:
+               root.right = abb(key)
+           else:
+               abb_insert_encuestados(root.right, key)  
+    
+    if root.val.getOpinion()<key.getOpinion():
+        if root.right == None:
+            root.right = abb(key)
+        else:
+            abb_insert_encuestados(root.right, key)
+
+#Sirve para imprimir la id de los encuestados en orden ascendiente
+def inorder_encuestados(root):
+    if root is None:
+        return root
+    inorder_encuestados(root.right)
+    print(root.val.getID(), end=" ")
+    inorder_encuestados(root.left)
+
+root = abb_encuestados(lista)
+print("Encuestados de la pregunta 1 (con abb): ")
+inorder_encuestados(root)
+print(" ")
 
 #Punto 2
 
