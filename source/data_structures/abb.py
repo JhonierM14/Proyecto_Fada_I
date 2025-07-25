1ddc9f1b069932a96a3ce9040f7d298c3e803cca
David Santiago Guerrero <144239804+DavidSantiagoGuerrero@users.noreply.github.com>
2025-07-25 09:45:09 -0500

    Update abb.py
    Funciones auxiliares de abb

--- source/data_structures/abb.py
+++ source/data_structures/abb.py
@@ -50,3 +50,39 @@ def inorder(root: abb):
     inorder(root.right)
     print(root.val.getNombre(), end=" ")
     inorder(root.left)
+
+def Arb_Size(arbol):
+    if arbol is None:
+        return 0
+    return 1 + Arb_Size(arbol.left) + Arb_Size(arbol.right)
+
+def Arb_to_List(nodo, resultado): # Esta función pasa un arbol a una lista
+        if nodo is None:
+            return 0
+        Arb_to_List(nodo.left, resultado)
+        resultado.append(nodo.val)
+        Arb_to_List(nodo.right, resultado)
+    
+def insertion_sort(arr, metodo):
+    for i in range(1, len(arr)):
+        key_item = arr[i]
+        key = metodo(key_item)
+        j = i - 1
+        while j >= 0 and metodo(arr[j]) > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key_item
+    return arr
+
+def Arb_Median(arbol, metodo):
+    elementos = []
+    Arb_to_List(arbol, elementos)
+    elementos = insertion_sort(elementos, metodo)
+    n = len(elementos)
+
+    if n == 0:
+        return None
+    if n % 2 == 1:
+        return elementos[n // 2]
+    else:
+        return elementos[n // 2 - 1]
